145963f37b60939c25f9bf35cc56ee9d6d7c15d4
s1gmas1x <sigma_vi@yahoo.com>
2025-12-02 16:42:59 -0700

    Added GA and adjusted font

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,13 +4,40 @@ export default defineConfig({
   title: 'KitchenRatio',
   description: 'Recipes, guides, and baker’s percentages - bake smarter with KitchenRatio.',
 
+   head: [
+    // Google Analytics
+    [
+      'script',
+      { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-T4SQXKGZK8' }
+    ],
+    [
+      'script',
+      {},
+      `window.dataLayer = window.dataLayer || [];
+       function gtag(){dataLayer.push(arguments);}
+       gtag('js', new Date());
+       gtag('config', 'G-T4SQXKGZK8');`
+    ],
+
+    // Google Fonts
+    [
+      'link',
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap' }
+    ],
+    [
+      'link',
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap' }
+    ]
+  ],
+
+
   sitemap: {
     hostname: 'https://kitchenratio.com'
   },
 
   themeConfig: {
     logo: '/logo2darktrans.svg',
-    darkLogo: '/logo2lighttrans.svg',
+    
     nav: [
       { text: 'Home', link: '/' },
       { text: 'How-To Guides', link: '/guides/' },
